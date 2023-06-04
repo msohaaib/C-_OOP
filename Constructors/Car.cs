e6104136d030d802fe8389b8d4d4321d836b4289
Muhammad Sohaib <114578522+msohaaib@users.noreply.github.com>
2023-06-04 04:44:04 -0700

    Add files via upload

---
+++ Constructors/Car.cs
@@ -0,0 +1,25 @@
+﻿// with CONSTRUCTOR
+
+//class Car
+//{
+//    public string carName;
+//    public string carColor;
+//    public int carModel;
+
+
+//    public Car(string name, string color, int model)
+//    {
+//        carName = name;
+//        carColor = color;
+//        carModel = model;
+//    }
+//}
+
+
+// without CONSTRUCTOR
+class Car
+{
+    public string carName;
+    public string carColor;
+    public int carModel;
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // with CONSTRUCTOR
+
+            //Car car = new Car("mustang", "Black", 2017);
+            //Car sports = new Car("Ferrari", "Red", 2015);
+            //Car royal = new Car("Rolls-Royce", "Silver", 1964);
+
+            //Console.WriteLine($"I have {car.carColor} {car.carName} {car.carModel} model.");
+            //Console.WriteLine($"I have {sports.carColor} {sports.carName} {sports.carModel} model.");
+            //Console.WriteLine($"I have {royal.carColor} {royal.carName} {royal.carModel} model.");
+            //Console.ReadLine();
+
+
+            // without CONSTRUCTOR
+
+            Car car = new Car();
+            car.carName = "mustang";
+            car.carColor = "Black";
+            car.carModel = 2015;
+            Console.WriteLine($"i have {car.carColor} {car.carName} {car.carModel} model.");
+            Console.ReadLine();
+        }
+        }
+}
+

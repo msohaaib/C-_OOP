bae9bf8a7415f2daab5cf24dd1dfc13c28338a9b
Muhammad Sohaib <114578522+msohaaib@users.noreply.github.com>
2023-06-05 22:23:30 -0700

    Add files via upload

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //account myAccount = new account();
+            //myAccount.setBalance(100000);
+            //myAccount.getBalance();
+            //Console.ReadLine();
+
+
+
+
+
+
+            Console.WriteLine("What you want from your account ?");
+            string answer = Console.ReadLine();
+            account myAccount = new account();
+            if (answer == "add")
+            {
+                Console.WriteLine("Enter amount :");
+                int number = int.Parse(Console.ReadLine());
+                myAccount.setBalance(1000 + number);
+                myAccount.getBalance();
+            }
+            else if(answer == "sub")
+            {
+                Console.WriteLine("Enter amount :");
+                int number = int.Parse(Console.ReadLine());
+                myAccount.setBalance(1000 - number);
+                myAccount.getBalance();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+        }
+    }
+}
---
+++ Encapsulation/account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class account
+{
+    private int accountBalance = 1000;
+
+    public void setBalance(int amout)
+    {
+        accountBalance = amout;    
+    }
+    public void getBalance()
+    {
+        Console.WriteLine($"Your account balance is {accountBalance}");
+    }
+}

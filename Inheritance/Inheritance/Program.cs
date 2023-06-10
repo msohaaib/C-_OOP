d0e49bc7b24c61cdc99868d3fafbdb573200783c
Muhammad Sohaib <114578522+msohaaib@users.noreply.github.com>
2023-06-10 06:03:48 -0700

    Add files via upload

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            std.name = "Sohaib";
+            std.mail = "Sohaib@hotmail.com";
+            std.phone = 4321;
+            std.English_marks = 85;
+            std.Urdu_marks = 80;
+            std.Computer_marks = 100;
+            Console.WriteLine($"Name = {std.name}\nmail = {std.mail}\nphone = {std.phone}\nTotal Marks = {std.English_marks + std.Urdu_marks + std.Computer_marks}");
+            
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Result 
+    {
+        public int English_marks;
+        public int Urdu_marks;
+        public int Computer_marks;
+    }
+}

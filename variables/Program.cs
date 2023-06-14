c3bb6de997cfaebc73cd44589ceaf51a560640e3
Muhammad Sohaib <114578522+msohaaib@users.noreply.github.com>
2023-06-14 07:27:12 -0700

    Add files via upload

---
+++ variables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace program
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Please Enter Your Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("your Name IS" + " " + name);
+        }
+    }
+}
